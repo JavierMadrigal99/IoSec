d9882ef159a41c8c05b141cbe48ee317331bb2c8
JavierMadrigal99 <58833463+JavierMadrigal99@users.noreply.github.com>
2024-12-20 23:41:55 -0600

    Add files via upload

---
+++ Prueba/script.js
@@ -0,0 +1,9 @@
+// Detecta el scroll para ajustar la barra de navegación
+window.addEventListener('scroll', function () {
+    const navbar = document.getElementById('navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('shrink');
+    } else {
+        navbar.classList.remove('shrink');
+    }
+});
